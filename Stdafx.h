2f77af5834cd7aca84eb120c2acc7ebffe9fb2c6
PetrovMaksym <61542889+PetrovMaksym@users.noreply.github.com>
2020-05-06 23:12:39 +0300

    Add files via upload

---
+++ Stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <ctime>
+#include <sstream>
+#include "low.h"
+#include "midle.h"
+#include "hard.h"
+///
+///
